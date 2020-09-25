b42a121cd08a6948ece35e34673a5d35f1339366
Pierre56320 <39170490+Pierre56320@users.noreply.github.com>
2020-09-25 14:55:21 +0200

    [MAJ] data_process.c avec la modif drone_data

--- software/appli/datas_process.c
+++ software/appli/datas_process.c
@@ -8,14 +8,11 @@
 
 
 #include "datas_process.h"
+#include "stm32f4_timer.h"
 
 
 
-datas_sensors_pooling_t datas_sensors_pooling;
-datas_drone_position_t datas_drone_position;
-
-datas_sensors_pooling_t* p_datas_sensors_pooling = &datas_sensors_pooling;
-datas_drone_position_t* p_datas_drone_position = &datas_drone_position;
+drone_data_t* drone_data;
 
 bool_e TOF_OK = 0;
 uint32_t compteur_no_pooling_tof = 0;
@@ -33,8 +30,6 @@ void TIMER2_user_handler_it_1ms(void)
 //compteur declar� dans tests_methods.h
 	if(compt <= 5000){ compt += 1; }
 	else{ compt = 0; }
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//Fonction d'interrogation des ToFs toutes les TIME_NO_POOLING_TOF ms, si en communication TOF_OK = 0, sinon, TOF_OK = 1
 
 	if(compteur_no_pooling_tof <= TIME_MS_POOLING_TOF && TOF_OK ==1){
@@ -55,9 +50,10 @@ void TIMER2_user_handler_it_1ms(void)
 		else{
 			TOF_OK = 0; //sinon communication pas finie =>
 		}
+		datas_tof_maj();
 
 	}
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	if(compteur_no_pooling_mpu <= TIME_MS_POOLING_MPU){
 		compteur_no_pooling_mpu++;
 	}
@@ -65,92 +61,29 @@ void TIMER2_user_handler_it_1ms(void)
 		MPU_angle_computer();
 		compteur_no_pooling_mpu = 0;
 	}
-
-
-
-	//switch(timeslot)... //si l'on souhaite conditionner certaines choses aux timeslots des VL_53... pour r�partir la charge de calcul dans le temps.
-
-	//notamment pour discuter avec le MPU6050 ! (car il utilise le m�me bus I2C ! donc il faut uniquement lui causer en IT !)
 }
 
 
-uint8_t data_process_main(){
-	static state_e state;
-	state = INIT_POSITION;
-	static uint8_t DATAS_STRUCT_POSITION_OK = 0;
-
-	switch (state) {
-		case INIT_POSITION:
-			//.................init()
-			state = READ_AND_PROCESS_DATA;
-			break;
-		case READ_AND_PROCESS_DATA:
-			if(TOF_OK){
-				//maj nos datastrcut r�elles dans datas_sensors_pooling
-				datas_tof_maj();
-				process_data();
-			}
-			//state = PROCESS_DATA;
-			DATAS_STRUCT_POSITION_OK = 1;
-
-			break;
-		case STOP :
-			break;
-
-	}
-
-	return DATAS_STRUCT_POSITION_OK;
+void data_process_init(drone_data_t * drone){
+	drone_data = drone;
+	MPU_init(drone_data);
+	VL53L1X_init();
+	TIMER2_run_1ms();
 }
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 void datas_tof_maj(){
-	datas_sensors_pooling.dist_0 = VL53L1X_get_distance(0);
-	datas_sensors_pooling.dist_1 = VL53L1X_get_distance(1);
-	datas_sensors_pooling.dist_2 = VL53L1X_get_distance(2);
-	datas_sensors_pooling.dist_3 = VL53L1X_get_distance(3);
-	datas_sensors_pooling.dist_4 = VL53L1X_get_distance(4);
+	drone_data->datas_sensors_pooling.dist_0 = VL53L1X_get_distance(0);
+	drone_data->datas_sensors_pooling.dist_1 = VL53L1X_get_distance(1);
+	drone_data->datas_sensors_pooling.dist_2 = VL53L1X_get_distance(2);
+	drone_data->datas_sensors_pooling.dist_3 = VL53L1X_get_distance(3);
+	drone_data->datas_sensors_pooling.dist_4 = VL53L1X_get_distance(4);
 }
 
-/*
- *@brief
- *
- */
 
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void process_data(){
-	//gyro
-	datas_drone_position.angle_X_ROLL += datas_sensors_pooling.Gyroscope_X;
-	datas_drone_position.angle_Y_PITCH += datas_sensors_pooling.Gyroscope_Y;
-	datas_drone_position.angle_Z_YAW += datas_sensors_pooling.Gyroscope_Z;
-
-	datas_drone_position.angle_X_ROLL /= 16400; //convertir en �
-	datas_drone_position.angle_Y_PITCH /= 16400; //convertir en �
-	datas_drone_position.angle_Z_YAW /= 16400; //convertir en �
-
-	//tofs
-	//datas_drone_position.position_X = datas_sensors_pooling.CAT(dist_, TOF_ID_AXE_X);
-
-	datas_drone_position.position_X = datas_sensors_pooling.dist_0 - datas_drone_position.X0 - MESURE_LARGEUR_DRONE_CM;
-	datas_drone_position.position_Y = datas_sensors_pooling.dist_1 - datas_drone_position.Y0 - MESURE_LARGEUR_DRONE_CM;
-	datas_drone_position.position_Z = datas_sensors_pooling.dist_2 - datas_drone_position.Z0 - MESURE_HAUEUR_DRONE_CM;
-
-
-}
-
-void init_datas(){
-	datas_drone_position.X0 = 0;
-	datas_drone_position.Y0 = 0;
-	datas_drone_position.Z0 = 0;
-
-
-//...................................
-
-
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
