a57ef3bdc47513b6fe32c33a9937201821ab2bd2
Pierre56320 <39170490+Pierre56320@users.noreply.github.com>
2020-09-26 19:58:56 +0200

    Revert "secretary"
    This reverts commit 8297078a9baa944d47c74f71ba7e27e83f0e49bb.

--- software/appli/bluetooth.h
+++ software/appli/bluetooth.h
@@ -19,6 +19,5 @@ void BLUETOOTH_envoi_trame(uint8_t info1, uint8_t info2, uint8_t info3, uint8_t 
 void BLUETOOTH_envoi_caractere(uint8_t c);
 void BLUETOOTH_reception_caractere(void);
 void BLUETOOTH_reception_trame(void);
-void BLUETOOTH_process_main(void);
 
 #endif /* APPLI_BLUETOOTH_H_ */
