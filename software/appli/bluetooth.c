8297078a9baa944d47c74f71ba7e27e83f0e49bb
Skyflowed <62890017+Skyflowed@users.noreply.github.com>
2020-09-26 18:57:21 +0200

    secretary

--- software/appli/bluetooth.c
+++ software/appli/bluetooth.c
@@ -6,7 +6,6 @@
  *      Author: martin
  */
 
-
 #include "bluetooth.h"
 
 
@@ -20,10 +19,10 @@
 void BLUETOOTH_init(void){
 	UART_init(UART2_ID, 38400);
 }
+
 //fonction
 //envoie donn�e PC sous forme d'une trame de 4 informations
 //Rajouter la fonction UART_puts(..,..,..) dans la librairie de l'UART + definir des variables
-
 void BLUETOOTH_envoi_trame(uint8_t info1, uint8_t info2, uint8_t info3, uint8_t info4){
 	static uint8_t trame[4];
 	trame[0] = info1;
@@ -60,4 +59,105 @@ void BLUETOOTH_reception_trame(void){
 	}
 }
 
+/*------------------- SECRETARY BLUETOOTH -------------------*/
+
+#define BUFFER_SIZE	128
+static void find_command(char * buffer);
+static void echo_command(char * buffer);
+static bool_e str_begin_with(char * string, char * begin);
+
+
+/**
+* @brief R�cup�re les octets re�us sur l� UART2, les rassemble dans un buffer et les traite si on re�oit '\n'
+* @pre cette fonction doit �tre appel�e r�guli�rement dans la t�che de fond
+* @pre les trames re�ues doivent se terminer par un '\n' et ne pas d�passer BUFFER_SIZE
+* @post la taille du buffer est limit�e. Si de trop nombreux caract�res sont re�us, ils s'�crasent � la fin.
+* @post le traitement des trames re�ues est sous-trait�
+*/
+void BLUETOOTH_process_main(void)
+{
+	static char buffer[BUFFER_SIZE];
+	static uint8_t index = 0;
+	char c;
+	while(UART_data_ready(UART2_ID))
+	{
+		c = UART_getc(UART2_ID);
+		buffer[index] = c;
+		if(c =='\n')
+		{
+			buffer[index] = '\0';
+			find_command(buffer);
+			index = 0;
+		}
+		else if(index<BUFFER_SIZE-1)
+		{
+			index += 1;
+		}
+	}
+}
+
+/**
+* @brief 	Fonction priv�e qui analyse le contenu de la chaine string pour voir si elle commence par le contenu de la chaine begin
+* @param	string : la chaine dans laquelle on recherche
+* @param	begin : la chaine que l'on recherche
+* @pre		begin doit �tre de taille inf�rieure � string
+* @ret		TRUE si string commence par begin. FALSE sinon.
+*/
+static bool_e str_begin_with(char * string, char * begin)
+{
+	bool_e ret;
+	uint16_t i;
+	ret = TRUE;
+	for(i=0;begin[i]!=0;i++)
+	{
+		if(string[i]==0 || string[i]!=begin[i])
+		{
+			ret = FALSE;
+			break;
+		}
+	}
+	return ret;
+}
+
+
+static void find_command(char * buffer)
+{
+	bool_e badstring, start, stop;
+	start = FALSE;
+	stop = FALSE;
+	badstring = FALSE; //On fait l'hypoth�se que la chaine est correcte.
+
+	if(str_begin_with(buffer, "start"))
+	{
+		//On lance le programme via bluetooth
+		//Créer un état wait for launching ds machine à états générale
+		start = TRUE;
+	}
+	else if(str_begin_with(buffer, "stop"))
+	{
+		//On stoppe tous les moteurs via bluetooth
+		MC_put_all_motors_off();
+		stop = TRUE;
+	}
+	else if(str_begin_with(buffer, "help"))
+	{
+		printf("--- commands list ---\n");
+		printf("start (To launch the program\n");
+		printf("stop  (To stop all motors\n");
+		printf("\"help\\n\" - show this commands list \n");
+	}
+	else
+		badstring = TRUE;
+	if(start)
+		printf("PROGRAM LAUNCHED\n");
+	if(stop)
+		printf("MOTORS STOPPED\n");
+	if(badstring)
+		printf("Bad command -> press help to list commands\n");
+}
+
+static void echo_command(char * buffer)
+{
+	puts(buffer);
+}
 
